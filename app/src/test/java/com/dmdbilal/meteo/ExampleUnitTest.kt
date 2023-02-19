b58c071d0937e67fcc686e33362975a159ec98ec
Mohamed Bilal <dmdbilal8604@gmail.com>
2023-02-19 11:18:27 +0530

    Added unit test

--- app/src/test/java/com/dmdbilal/meteo/ExampleUnitTest.kt
+++ app/src/test/java/com/dmdbilal/meteo/ExampleUnitTest.kt
@@ -1,17 +1,18 @@
 package com.dmdbilal.meteo
 
+import com.dmdbilal.meteo.data.di.AppModule.provideWeatherApi
+import com.dmdbilal.meteo.data.remote.WeatherApi
+import org.junit.Before
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+    private lateinit var api: WeatherApi
+    @Before
+    fun setup() {
+        api = provideWeatherApi()
+    }
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `test retrofit`() {
+        assert(api != null)
     }
 }
